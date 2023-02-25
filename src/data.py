d62fd8502ebd4bc85f4815ea498a82717f018ad3
DanielRoeder1 <roeder_daniel_97@web.de>
2023-02-25 15:09:03 +0100

    replace partial for better performance

--- src/data.py
+++ src/data.py
@@ -1,5 +1,4 @@
 import random 
-from functools import partial
 from torch.utils.data import DataLoader
 
 
@@ -14,19 +13,23 @@ def transform_func(example):
     query_text = " ".join(split_text[seq_begin:seq_begin+seq_len]) 
     return {"query": [query_text], "doc":[doc_text]}
 
-def custom_collate(batch_data, tokenizer1, tokenizer2):
-  """
-  Custom collate function to tokenize query and docs
-  """
-  queries = [dct["query"] for dct in batch_data]
-  docs = [dct["doc"] for dct in batch_data]
+# Faster compared to using partial
+# https://stackoverflow.com/questions/57822851/performant-way-to-partially-apply-in-python
+def outer_collate(tokenizer1, tokenizer2):
+  def custom_collate(batch_data):
+    """
+    Custom collate function to tokenize query and docs
+    """
+    queries = [dct["query"] for dct in batch_data]
+    docs = [dct["doc"] for dct in batch_data]
 
-  q_tokens = tokenizer1(queries, padding = True, return_tensors = "pt", truncation =True)
-  d_tokens = tokenizer2(docs, padding = True, return_tensors = "pt", truncation = True )
+    q_tokens = tokenizer1(queries, padding = True, return_tensors = "pt", truncation =True)
+    d_tokens = tokenizer2(docs, padding = True, return_tensors = "pt", truncation = True )
 
-  return q_tokens, d_tokens
+    return q_tokens, d_tokens
+  return custom_collate
 
 def get_data_loader(dataset, tokenizer1, tokenizer2, batch_size, shuffle = True):
   dataset.set_transform(transform_func)
-  collate_fn = partial(custom_collate, tokenizer1 = tokenizer1, tokenizer2 = tokenizer2)
+  collate_fn = outer_collate(tokenizer1, tokenizer2)
   return DataLoader(dataset, batch_size = batch_size, shuffle = shuffle, collate_fn = collate_fn)
