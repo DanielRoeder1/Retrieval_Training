ee13bbbdf7fca5a4f67b85a310fec52a0bb01157
julio-py <63744748+julio-py@users.noreply.github.com>
2023-03-28 19:59:59 +0200

    test poly_encoder implementation

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class BiEncoder(nn.Module):
-    def __init__(self, q_model, d_model):
+    def __init__(self, q_model = None, d_model = None):
         super(BiEncoder, self).__init__()
         self.q_model = q_model
         self.d_model = d_model
@@ -15,8 +15,54 @@ class BiEncoder(nn.Module):
 
         return q_embeds, d_embeds
 
-
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+# ------------------ New Model architectures ------------------ #
+from transformers import AutoModel, AutoConfig, PreTrainedModel
+import torch.nn.functional as F
+
+class GeneralizedPreTrainedModel(PreTrainedModel):
+    """
+    Abstract class that allows to load a pretrained model without specifying the model class.
+    """
+    config_class = AutoConfig
+    base_model_prefix = "enc_model"
+    def _init_weights(self, module):
+        return getattr(self, self.base_model_prefix, self)._init_weights(module)
+
+class PooledEncoder(GeneralizedPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.enc_model = AutoModel.from_config(config)
+
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    
+    def forward(self, inputs):
+        model_output = self.enc_model(**inputs)
+        return self.mean_pooling(model_output, inputs['attention_mask'])
+    
+class PolyEncoder(GeneralizedPreTrainedModel):
+    def __init__(self, config, num_codes):
+        super().__init__(config)
+        self.enc_model = AutoModel.from_config(config)
+        self.register_buffer('poly_codes', torch.normal(0,1,(num_codes, config.hidden_size), requires_grad=True))
+        self.poly_m = num_codes
+
+    def dot_attention(self, q, k, v):
+        attn_weights = torch.matmul(q, k.transpose(2, 1)) 
+        attn_weights = F.softmax(attn_weights, -1)
+        output = torch.matmul(attn_weights, v)
+        return output
+    
+    def forward(self, inputs, pooled_embeds):
+        q_embeds = self.enc_model(**inputs)[0] # [bs, length, dim]
+        poly_codes = self.poly_codes.expand(q_embeds.shape[0],-1,-1) # [bs, poly_m, dim]
+        poly_heads = self.dot_attention(poly_codes,q_embeds, q_embeds) # [bs, poly_m, dim]
+        outputs = self.dot_attention(pooled_embeds, poly_heads, poly_heads) # [doc_cnt, q_cnt, dim]
+        return outputs
---
+++ src/training_wrapper.py
@@ -0,0 +1,32 @@
+import torch
+from test_poly import CustomNTXentLoss
+import numpy as np
+
+class PolyEncoderWrapper(torch.nn.Module):
+    def __init__(self, pool_enc, cond_enc, args):
+        super().__init__()
+        self.pool_enc = pool_enc
+        self.cond_enc = cond_enc
+        self.loss_func = CustomNTXentLoss(temperature=0.07)
+
+        self.indices_tuples = self.get_indices_tuple(args.training.batch_size, args.training.num_pos)
+    
+    def forward(self, inputs):
+        q_inputs, d_inputs = inputs
+        d_embeds = self.pool_enc(d_inputs)
+        q_embeds = self.cond_enc(q_inputs, d_embeds)
+        q_embeds = q_embeds.reshape(-1, q_embeds.shape[-1])
+        loss = self.loss_func(embeddings = d_embeds,ref_emb = q_embeds,  indices_tuple = self.indices_tuples)
+        return loss
+        
+    def get_indices_tuple(self, num_doc, num_pos):
+        """
+        Creating custom indces_tuples for pytorch_metric_learning loss function
+        Used for building the positive and negative pairs
+        """
+        num_q = num_doc * num_pos
+        doc_pos = torch.arange(num_doc).repeat_interleave(num_pos)
+        pos_id = doc_pos + torch.arange(num_q) * num_doc
+        neg_id = torch.from_numpy(np.delete(np.arange(num_q*num_doc),pos_id)).long()
+        doc_neg = neg_id % num_doc
+        return doc_pos, pos_id, doc_neg, neg_id
